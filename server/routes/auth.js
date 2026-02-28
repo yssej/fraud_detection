1120319e73c7c6de5255b6a298461dd42b4ebe48
Jessy <jessyowen48@gmail.com>
2026-02-28 15:31:03 +0300

    feat: add refresh token endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 
 router.route('/login').post(authController.loginUser);
 router.route('/register').post(authController.registerUser);
+router.route('/refresh').post(authController.refreshToken)
 
 module.exports = router;
